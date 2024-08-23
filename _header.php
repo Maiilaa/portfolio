0a22bee0dde876dab804bb016ed29c808dc18f7f
Maiilaa <zirarimaila8@gmail.com>
2024-08-23 12:00:49 +0200

    12h

--- _header.php
+++ _header.php
@@ -3,16 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portfolio</title>
+    <title>Document</title>
     <link rel="stylesheet" href="Assets/css/index.css?t=<?=time()?>">
 </head>
 <body>
-   <?php include  '_header.php';?>
-    <main>
-        <img  class=""src="Assets/images/degrade.jpg" alt="image degrade">
-    </main>
-    <footer>
-
-    </footer>
+    <header>
+    <ul>
+            <li>Accueil</li>
+            <li>?</li>
+            <li>A</li>
+            <li>Contact</li>
+        </ul>
+    </header>
 </body>
 </html>
